ba2075417e0c13d795b3ac806878a674dbf6aa66
Michele Avagliano <avaglianomichele@outlook.it>
2026-02-09 15:12:51 +0100

    Feat: added automatic control on logging behaviour in surrogate downloads

--- src/metatab/metalearning/load.py
+++ src/metatab/metalearning/load.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 from importlib.metadata import version
 from packaging.version import Version
 from packaging.specifiers import SpecifierSet
-from huggingface_hub import hf_hub_download
+from huggingface_hub import hf_hub_download, try_to_load_from_cache
 
 if TYPE_CHECKING:
     from sklearn.pipeline import Pipeline
@@ -22,23 +22,35 @@ def download_surrogate_framework(type_estimator: TunableEstimatorType) -> str:
     from the hugging face repo in the user hf cache directory.
     Returns the user system model filepath.
     '''
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logger.propagate=False
-    logger.handlers.clear()
-    handler = logging.StreamHandler(stream=sys.stdout)
-    handler.setLevel(logging.INFO)
-    logger.addHandler(handler)
+    models_subfolder = resolve_surrogate_models_folder()
 
-    logger.info(f"Attempting to download the surrogate model from HuggingFace.")
+    # we use this mechanisms based on the cache existence to decide when logging.
+    # we call in every case the "hf_hub_download" function as main hf function to do the job.
+    # the result of this call is a string if cache exists else None or _CACHED_NO_EXIST (not a string)
+    cache_existence: str | None = try_to_load_from_cache(
+        repo_id="piupo/metatab_surrogate_pipelines",
+        filename=f"{models_subfolder}/surrogate_framework_for_{type_estimator}.joblib",
+    )
+
+    if not isinstance(cache_existence, str):
+        logger = logging.getLogger(__name__ + "_" + type_estimator)
+        logger.setLevel(logging.INFO)
+        logger.propagate=False
+        logger.handlers.clear()
+        handler = logging.StreamHandler(stream=sys.stdout)
+        handler.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        logger.info(f"Attempting to download the surrogate model from HuggingFace.")
 
     model_filepath = hf_hub_download(
         repo_id="piupo/metatab_surrogate_pipelines",
         filename=f"surrogate_framework_for_{type_estimator}.joblib",
-        subfolder=resolve_surrogate_models_folder()
+        subfolder=models_subfolder
     )
 
-    logger.info("Download completed!")
+    if not isinstance(cache_existence, str):
+        logger.info("Download completed!")
+    
     return model_filepath
 
 
